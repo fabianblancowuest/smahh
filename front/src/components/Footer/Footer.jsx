7fb875eaf7d1be9a0a2526fdde927348dfe003af
LucianoAscuet <lucianoascuet@gmail.com>
2023-09-11 22:33:54 -0300

    footer links

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaTwitter, FaGithub, FaYoutube } from 'react-icons/fa';
 
+import './Footer.css';
+
 const navigation = {
     main: [
-        { name: 'About', href: '#' },
+        { name: 'About', href: '/about' },
         { name: 'Blog', href: '#' },
         { name: 'Jobs', href: '#' },
         { name: 'Press', href: '#' },
@@ -13,27 +15,27 @@ const navigation = {
     social: [
         {
             name: 'Facebook',
-            href: '#',
+            href: 'https://www.facebook.com',
             icon: <FaFacebook />,
         },
         {
             name: 'Instagram',
-            href: '#',
+            href: 'https://www.instagram.com',
             icon: <FaInstagram />,
         },
         {
             name: 'Twitter',
-            href: '#',
+            href: 'https://twitter.com',
             icon: <FaTwitter />,
         },
         {
             name: 'GitHub',
-            href: '#',
+            href: 'https://github.com', 
             icon: <FaGithub />,
         },
         {
             name: 'YouTube',
-            href: '#',
+            href: 'https://www.youtube.com', 
             icon: <FaYoutube />,
         },
     ],
@@ -41,26 +43,24 @@ const navigation = {
 
 export default function Footer() {
     return (
-        <footer style={{ backgroundColor: '#333', color: '#fff' }}>
-            <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
-                <nav style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }} aria-label="Footer">
+        <footer>
+            <div className="container">
+                <nav aria-label="Footer">
                     {navigation.main.map((item) => (
-                        <div key={item.name} style={{ marginRight: '20px' }}>
-                            <a href={item.href} style={{ color: '#fff', textDecoration: 'none', fontSize: '14px' }}>
-                                {item.name}
-                            </a>
+                        <div key={item.name}>
+                            <a href={item.href}>{item.name}</a>
                         </div>
                     ))}
                 </nav>
-                <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                <div className="social-links">
                     {navigation.social.map((item) => (
-                        <a key={item.name} href={item.href} style={{ color: '#ccc', textDecoration: 'none', margin: '0 10px' }}>
+                        <a key={item.name} href={item.href} target="_blank" rel="noopener noreferrer">
                             {item.icon}
                             <span className="sr-only">{item.name}</span>
                         </a>
                     ))}
                 </div>
-                <p style={{ textAlign: 'center', fontSize: '12px' }}>&copy; 2023 Smahh. All rights reserved.</p>
+                <p className="copyright">&copy; 2023 Smahh. All rights reserved.</p>
             </div>
         </footer>
     );
