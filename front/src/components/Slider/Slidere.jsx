24f636c9e0ff9d519b4085f7791d4ca31f4d98ea
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2023-09-14 18:05:24 -0300

    Bug fixed

--- front/src/components/Slider/Slidere.jsx
+++ front/src/components/Slider/Slidere.jsx
@@ -1,31 +1,32 @@
 import malwareRemoval from "../../assets/images/services/malware-removal.jpg";
 import cloudSecurity from "../../assets/images/services/cloud-security.jpg";
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.css";
+// import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/swiper-bundle.css";
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
+// SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 const Slidere = () => {
 	return (
-		<Swiper
-			spaceBetween={30}
-			slidesPerView={1}
-			navigation
-			pagination={{ clickable: true }}
-			autoplay={{ delay: 3000 }}
-		>
-			<SwiperSlide>
-				<div>
-					<h2>COntenido slide 1</h2>
-					<img src={malwareRemoval}></img>
-				</div>
-			</SwiperSlide>
-			<div>
-				<h2>COntenido slide 1</h2>
-				<img src={cloudSecurity}></img>
-			</div>
-		</Swiper>
+		<div>Nada</div>
+		// <Swiper
+		// 	spaceBetween={30}
+		// 	slidesPerView={1}
+		// 	navigation
+		// 	pagination={{ clickable: true }}
+		// 	autoplay={{ delay: 3000 }}
+		// >
+		// 	<SwiperSlide>
+		// 		<div>
+		// 			<h2>COntenido slide 1</h2>
+		// 			<img src={malwareRemoval}></img>
+		// 		</div>
+		// 	</SwiperSlide>
+		// 	<div>
+		// 		<h2>COntenido slide 1</h2>
+		// 		<img src={cloudSecurity}></img>
+		// 	</div>
+		// </Swiper>
 	);
 };
 
