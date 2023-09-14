6396ab0ec9beb1ec044b70c0751e0fbaf79757b9
LucianoAscuet <lucianoascuet@gmail.com>
2023-09-14 19:17:17 -0300

    searchBar staff with enter

--- front/src/components/SearchBar/SearchBar.jsx
+++ front/src/components/SearchBar/SearchBar.jsx
@@ -1,41 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { searchById, searchByName } from "../../redux/actions/actions";
-import styles from "./Searchbar.module.css"
+import styles from "./Searchbar.module.css";
 
 const SearchBar = () => {
-    const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
+  const inputRef = useRef(null); // Agrega una referencia al campo de entrada
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const handleChange = (event) => {
-        setSearch(event.target.value)
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSearch = () => {
+    if (!search) {
+      return alert("You must enter a search");
     }
-
-    const handleSearch = () => {
-        if (!search) {
-            return alert("You must enter search")
-        }
-        if (/^\d+$/.test(search)) {
-            dispatch(searchById(search))
-            setSearch("")
-        } else {
-            dispatch(searchByName(search))
-            setSearch("")
-        }
+    if (/^\d+$/.test(search)) {
+      dispatch(searchById(search));
+      setSearch("");
+    } else {
+      dispatch(searchByName(search));
+      setSearch("");
     }
+  };
 
-    return (
+  const handleKeyUp = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
 
-        <div className={styles.searchbar}>
-            <input type='text' className={styles.input} onChange={handleChange} value={search} placeholder="Enter your search" />
+  return (
+    <div className={styles.searchbar}>
+      <input
+        type="text"
+        className={styles.input}
+        onChange={handleChange}
+        value={search}
+        placeholder="Enter your search"
+        onKeyUp={handleKeyUp} // Manejador de eventos para la tecla "Enter"
+        ref={inputRef} // Referencia al campo de entrada
+      />
+      <button className={styles.searchButton} onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
+};
 
-            <button className={styles.searchButton} onClick={handleSearch}>Search</button>
-        </div>
-
-
-
-    )
-}
-
-export default SearchBar
+export default SearchBar;
