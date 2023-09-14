264d8e8219d377f531890a8f51439f653689ed23
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2023-09-14 18:02:34 -0300

    Landing was improved

--- front/src/components/Slider/Slidere.jsx
+++ front/src/components/Slider/Slidere.jsx
@@ -1,106 +1,31 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import malwareRemoval from "../../assets/images/services/malware-removal.jpg";
 import cloudSecurity from "../../assets/images/services/cloud-security.jpg";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 const Slidere = () => {
-	const settings = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-	};
-
 	return (
-		<div className="home-container">
-			{/* Resto del código */}
-			<article id="principal-main" className="home-principalMain">
-				<Slider {...settings}>
-					<div className="home-cards">
-						{/* Contenido de tu primera sección de cards */}
-						<a href="#cloud-access-security-broker">
-							<div className="home-card">
-								<h2 className="home-cardTitle">Cloud Access Security Broker</h2>
-								<p className="home-cardText">
-									Empowering your organization with control and visibility over
-									cloud environments.
-									<img className="home-img" src={cloudSecurity}></img>
-								</p>
-							</div>
-						</a>
-						<a href="#compilance-security-program">
-							<div className="home-card">
-								<h2 className="home-cardTitle">Compilance Security Program</h2>
-								<p className="home-cardText">
-									A comprehensive framework ensuring your organization meets and
-									exceeds industry-specific compliance standards
-								</p>
-								<img className="home-img" src={malwareRemoval}></img>
-							</div>
-						</a>
-						<a href="#compilance-security-program">
-							<div className="home-card">
-								<h2 className="home-cardTitle">Compilance Security Program</h2>
-								<p className="home-cardText">
-									A comprehensive framework ensuring your organization meets and
-									exceeds industry-specific compliance standards
-								</p>
-								<img className="home-img" src={malwareRemoval}></img>
-							</div>
-						</a>
-					</div>
-					<div className="home-cards">
-						{/* Contenido de tu segunda sección de cards */}
-						<a href="#compilance-security-program">
-							<div className="home-card">
-								<h2 className="home-cardTitle">Compilance Security Program</h2>
-								<p className="home-cardText">
-									A comprehensive framework ensuring your organization meets and
-									exceeds industry-specific compliance standards
-								</p>
-								<img className="home-img" src={malwareRemoval}></img>
-							</div>
-						</a>
-					</div>
-					<div>
-						<a href="#cloud-access-security-broker">
-							<div className="home-card">
-								<h2 className="home-cardTitle">Cloud Access Security Broker</h2>
-								<p className="home-cardText">
-									Empowering your organization with control and visibility over
-									cloud environments.
-									<img className="home-img" src={cloudSecurity}></img>
-								</p>
-							</div>
-						</a>
-						<a href="#compilance-security-program">
-							<div className="home-card">
-								<h2 className="home-cardTitle">Compilance Security Program</h2>
-								<p className="home-cardText">
-									A comprehensive framework ensuring your organization meets and
-									exceeds industry-specific compliance standards
-								</p>
-								<img className="home-img" src={malwareRemoval}></img>
-							</div>
-						</a>
-						<a href="#compilance-security-program">
-							<div className="home-card">
-								<h2 className="home-cardTitle">Compilance Security Program</h2>
-								<p className="home-cardText">
-									A comprehensive framework ensuring your organization meets and
-									exceeds industry-specific compliance standards
-								</p>
-								<img className="home-img" src={malwareRemoval}></img>
-							</div>
-						</a>
-					</div>
-					{/* ... y así sucesivamente */}
-				</Slider>
-			</article>
-		</div>
+		<Swiper
+			spaceBetween={30}
+			slidesPerView={1}
+			navigation
+			pagination={{ clickable: true }}
+			autoplay={{ delay: 3000 }}
+		>
+			<SwiperSlide>
+				<div>
+					<h2>COntenido slide 1</h2>
+					<img src={malwareRemoval}></img>
+				</div>
+			</SwiperSlide>
+			<div>
+				<h2>COntenido slide 1</h2>
+				<img src={cloudSecurity}></img>
+			</div>
+		</Swiper>
 	);
 };
 
