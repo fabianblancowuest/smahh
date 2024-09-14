489113b35588feb1a6d06f3b709d56d18b21a76a
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-09-14 01:33:07 -0300

    update index.js

--- back/src/index.js
+++ back/src/index.js
@@ -25,7 +25,7 @@ server.use((req, res, next) => {
 server.use("/", mainRouter);
 
 conn.sync({ force: false }).then(
-	server.listen(PORT, "0.0.0.0", () => {
+	server.listen(PORT, () => {
 		console.log("Server raised in port: " + PORT);
 	}),
 );
